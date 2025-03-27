493b0dee5240e9f10049b224591e319212b361bb
Vignesh27V <svmvignesh27@gmail.com>
2025-03-27 16:04:22 +0530

    Improve status bar hiding

--- app/src/main/java/com/example/favdish/SplashActivity.kt
+++ app/src/main/java/com/example/favdish/SplashActivity.kt
@@ -2,7 +2,9 @@ package com.example.favdish
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,14 +20,27 @@ class SplashActivity : AppCompatActivity() {
         splashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)//Set the UI content using view binding
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){//checks if the version is above Android 11+
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){//checks if the version is above Android 11+
             window.insetsController?.hide(WindowInsets.Type.statusBars())//hide status bar
         }else{//For Pre_Android 11+
             window.setFlags(//hide status bar // setFlags(int flags, int mask)
                 WindowManager.LayoutParams.FLAG_FULLSCREEN, // flags = The flag to be set (FLAG_FULLSCREEN).
                 WindowManager.LayoutParams.FLAG_FULLSCREEN // masks = Specifies which flag(s) should be affected.
                  )//Passing the same flag twice ensures that only the FLAG_FULLSCREEN flag is updated.
+        }*/
+        //improved version of above commented code
+        //this if else is for support backward compatibility
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.apply {
+                hide(WindowInsets.Type.statusBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE //  temporarily appear when the user swipes from the edges.
+            }
+        } else {
+            // @Suppress("DEPRECATION") suppresses the warning that systemUiVisibility is deprecated in API 30+.
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN // hides the status bar
         }
+
 
     }
 
